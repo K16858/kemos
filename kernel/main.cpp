41ada8d3324e6bde6535e050a759d1160a5708c0
k16858 <ktohda0127@gmail.com>
2025-10-18 15:00:15 +0900

    インデント修正

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -3,7 +3,7 @@
 extern "C" void KernelMain(uint64_t framebuffer_base, uint64_t framebuffer_size) {
     uint8_t* frame_buffer = reinterpret_cast<uint8_t*>(framebuffer_base);
     for (uint64_t i = 0; i < framebuffer_size; i++) {
-        frame_buffer[i] = i % 256;
+            frame_buffer[i] = i % 256;
     }
     while (1) __asm__("hlt");
 }
