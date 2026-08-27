4603414ec55192149606746d6658305bf606c93f
k16858 <ktohda0127@gmail.com>
2026-08-28 01:29:14 +0900

    feat: キーボード割り込みを有効化

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -6,6 +6,7 @@
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
 #include "interrupt.hpp"
+#include "keyboard.hpp"
 #include "pic.hpp"
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
@@ -38,8 +39,9 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   __asm__("int $0x40");
 
   InitializePIC();
+  InitializeKeyboard(&console);
   __asm__("sti");
-  console.PutString("PIC ok, sti\n");
+  console.PutString("keyboard irq ready\n");
 
   while (1) __asm__("hlt");
 }
