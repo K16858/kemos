09813d9af6941a9181c4bf83a28ee87842e5f0e4
k16858 <ktohda0127@gmail.com>
2026-08-28 01:01:12 +0900

    feat: PIC初期化を追加

---
+++ kernel/pic.cpp
@@ -0,0 +1,36 @@
+#include "pic.hpp"
+
+#include "asmfunc.h"
+
+namespace {
+
+constexpr uint16_t kPic1Command = 0x0020;
+constexpr uint16_t kPic1Data = 0x0021;
+constexpr uint16_t kPic2Command = 0x00a0;
+constexpr uint16_t kPic2Data = 0x00a1;
+
+}  // namespace
+
+void InitializePIC() {
+  IoOut8(kPic1Data, 0xff);
+  IoOut8(kPic2Data, 0xff);
+
+  IoOut8(kPic1Command, 0x11);
+  IoOut8(kPic2Command, 0x11);
+
+  IoOut8(kPic1Data, 0x20);
+  IoOut8(kPic2Data, 0x28);
+
+  IoOut8(kPic1Data, 1u << 2);
+  IoOut8(kPic2Data, 2);
+
+  IoOut8(kPic1Data, 0x01);
+  IoOut8(kPic2Data, 0x01);
+
+  IoOut8(kPic1Data, 0xff);
+  IoOut8(kPic2Data, 0xff);
+}
+
+void NotifyEndOfInterrupt() {
+  IoOut8(kPic1Command, 0x20);
+}
